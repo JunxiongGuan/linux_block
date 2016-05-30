powervm/ibmvscsis

block: Export blk_poll
Without exporting blk_poll we get a compilation error.

Signed-off-by: Sagi Grimberg <sagig@mellanox.com>

block/blk-core.c
@@ -3373,6 +3373,7 @@ bool blk_poll(struct request_queue *q, blk_qc_t cookie)
 
 	return false;
 }
+EXPORT_SYMBOL(blk_poll);
 
 #ifdef CONFIG_PM
 /**

JunxiongGuan/linux_block

block/blk-core.c
@@ -3377,6 +3377,7 @@ bool blk_poll(struct request_queue *q, blk_qc_t cookie)
 
 	return false;
 }
+EXPORT_SYMBOL_GPL(blk_poll);
 
 #ifdef CONFIG_PM
 /**